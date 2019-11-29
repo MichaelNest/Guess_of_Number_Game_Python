919e21ef682ff7ca1b79d7086993db0e122c35d7
Michael Nesterenko <nesterenkom385@gmail.com>
2019-11-29 16:50:49 +0200

    Commit 2 - Внутренний и Главный циклы

--- guess_of_number_game.py
+++ guess_of_number_game.py
@@ -13,18 +13,29 @@ start_score = 100
 max_score = 0
 score = 0
 
-digit = random.randint(low_digit, high_digit)
-print('Компьютер загадал число - попробуй его отгадать!!')
-print(f'Guess computer number is: {digit}')
 
-while (not x.isdigit()):
-    x = input(f'Введите число от {low_digit} до {high_digit}: ')
-    if (not x.isdigit()):
-        print('.'*22 + 'Вы ввели какую-то ересь. Введите пожалуйста число!!'+'.'*22)
-x = int(x)
+while(play_game):
+    digit = random.randint(low_digit, high_digit)
+    print('Компьютер загадал число - попробуй его отгадать!!')
+    print(f'Guess computer number is: {digit}')
+    while (not win):
+        x = ''
+        while (not x.isdigit()):
+            x = input(f'Введите число от {low_digit} до {high_digit}: ')
+            if (not x.isdigit()):
+                print('.' * 22 + 'Вы ввели какую-то ересь. Введите пожалуйста число!!' + '.' * 22)
+        x = int(x)
 
-if(x == digit):
-    print('Победа!! Поздравляем!!')
+        if (x == digit):
+            print('Победа!! Поздравляем!!')
+            win = True
+    if (input('Enter - сыграть еще; 0 - выход') == '0'):
+        play_game = False
+    else:
+        win = False
+
+
+
 
 
 

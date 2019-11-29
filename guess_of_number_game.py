140a15ab14c712597880ec34f422b4eb7ef4ddff
Michael Nesterenko <nesterenkom385@gmail.com>
2019-11-29 16:15:22 +0200

    Commit 1 - Создаем каркас игры

---
+++ guess_of_number_game.py
@@ -0,0 +1,35 @@
+import random
+
+
+low_digit = 10
+high_digit = 50
+digit = 0
+
+count_input = 0
+x= ''
+play_game = True
+win = False
+start_score = 100
+max_score = 0
+score = 0
+
+digit = random.randint(low_digit, high_digit)
+print('Компьютер загадал число - попробуй его отгадать!!')
+print(f'Guess computer number is: {digit}')
+
+while (not x.isdigit()):
+    x = input(f'Введите число от {low_digit} до {high_digit}: ')
+    if (not x.isdigit()):
+        print('.'*22 + 'Вы ввели какую-то ересь. Введите пожалуйста число!!'+'.'*22)
+x = int(x)
+
+if(x == digit):
+    print('Победа!! Поздравляем!!')
+
+
+
+
+
+
+
+
